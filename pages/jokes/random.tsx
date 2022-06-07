4f0839efa4f63a16cc43644a44b807ff967e4a21
Nguyễn Hữu An <nguyenhuuan2727@gmail.com>
2022-06-07 15:36:58 +0700

    edit getServerSideProps

--- pages/jokes/random.tsx
+++ pages/jokes/random.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import { getRandomJoke } from '../../lib/joke';
 import { InferGetServerSidePropsType } from 'next';
+import { GetServerSideProps } from 'next';
 
-type joke = {
+type Props = {
+  joke?: any;
   icon_url?: string;
   id?: string;
   url?: string;
   value?: string;
 };
 
-const RandomJoke = ({ joke }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+const RandomJoke: React.FC<Props> = ({ joke }) => {
   return <div>{joke.value}</div>;
 };
 
-export const getServerSideProps = async () => {
+export const getServerSideProps: GetServerSideProps = async () => {
   const joke = await getRandomJoke();
   return {
     props: {
