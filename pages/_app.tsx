238e299a7a8e5081330f085ca6c92c217acd7492
Nguyễn Hữu An <nguyenhuuan2727@gmail.com>
2022-06-07 10:24:24 +0700

    custom app component typescript

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import React from 'react';
 
-function MyApp({ Component, pageProps }: AppProps) {
+const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <div className="root-app">
       <Component {...pageProps} />
     </div>
   );
-}
+};
 
-export default MyApp;
+export default App;
